0fbcca4307ffe3a88fae952724a058cfa3e8bfbc
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-19 14:12:55 +0900

    Initial commit

---
+++ src/main/java/com/verystrong/car_loan_project/CarLoanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.verystrong.car_loan_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoanProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarLoanProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/verystrong/car_loan_project/CarLoanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verystrong.car_loan_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLoanProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
