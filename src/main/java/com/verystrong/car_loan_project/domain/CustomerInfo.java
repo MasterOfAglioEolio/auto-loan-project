1cf09745d58e04acfd648c987582c6f4eb94a479
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-21 04:01:51 +0900

    #1 - 유저정보 도메인 코드 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계했음
    생성자와 'equals()', 'hashcode()' 등이
    도메인 맥락에 맞게끔 디테이하게 설정함

--- src/main/java/com/verystrong/car_loan_project/domain/CustomerInfo.java
+++ src/main/java/com/verystrong/car_loan_project/domain/CustomerInfo.java
@@ -1,30 +1,126 @@
 package com.verystrong.car_loan_project.domain;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList="CustomerId"),
+        @Index(columnList="CustomerName")
+})
+@Entity
 public class CustomerInfo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long CustomerId;
+
+    @Column(nullable = false, length = 20) // 기본정보는 null false
+    private String CustomerName;
+    @Column(nullable = false)
     private Gender Gender;
+    @Column(nullable = false)
     private Long Age;
+
     private Education education;
     private MartialStatus martialStatus;
     private Integer FamilyCount;
     private Integer ChildCount;
+
+    @Setter  // 수정 가능한 데이터 Setter 설정
     private IncomeType incomeType;
-    private IncomeClass incomeClass;
-    private Occupation occupation;
-    private OrgType orgType;
-    private int EmployedYears;
-    private boolean HouseOwnedYN;
-    private DwellingType dwellingType;
-    private HosingType hosingType;
-    private ResidenceClass residenceClass;
-    private int HouseAge;
-    private boolean CarOwnedYN;
+    @Setter private IncomeClass incomeClass;
+    @Setter private Occupation occupation;
+    @Setter private OrgType orgType;
 
+    @Setter private int EmployedYears;
+    @Setter private boolean HouseOwnedYN;
+    @Setter private DwellingType dwellingType;
+    @Setter private HosingType hosingType;
+    @Setter private ResidenceClass residenceClass;
 
+    @Setter private int HouseAge;
+    @Setter private boolean CarOwnedYN;
 
+    private CustomerInfo(String customerName,
+                        Gender gender,
+                        Long age, Education education,
+                        MartialStatus martialStatus,
+                        Integer familyCount,
+                        Integer childCount,
+                        IncomeType incomeType,
+                        IncomeClass incomeClass,
+                        Occupation occupation,
+                        OrgType orgType,
+                        int employedYears,
+                        boolean houseOwnedYN,
+                        DwellingType dwellingType,
+                        HosingType hosingType,
+                        ResidenceClass residenceClass,
+                        int houseAge,
+                        boolean carOwnedYN) {
+        CustomerName = customerName;
+        Gender = gender;
+        Age = age;
+        this.education = education;
+        this.martialStatus = martialStatus;
+        FamilyCount = familyCount;
+        ChildCount = childCount;
+        this.incomeType = incomeType;
+        this.incomeClass = incomeClass;
+        this.occupation = occupation;
+        this.orgType = orgType;
+        EmployedYears = employedYears;
+        HouseOwnedYN = houseOwnedYN;
+        this.dwellingType = dwellingType;
+        this.hosingType = hosingType;
+        this.residenceClass = residenceClass;
+        HouseAge = houseAge;
+        CarOwnedYN = carOwnedYN;
+    }
 
+    public static CustomerInfo of(String customerName,
+                        Gender gender,
+                        Long age, Education education,
+                        MartialStatus martialStatus,
+                        Integer familyCount,
+                        Integer childCount,
+                        IncomeType incomeType,
+                        IncomeClass incomeClass,
+                        Occupation occupation,
+                        OrgType orgType,
+                        int employedYears,
+                        boolean houseOwnedYN,
+                        DwellingType dwellingType,
+                        HosingType hosingType,
+                        ResidenceClass residenceClass,
+                        int houseAge,
+                        boolean carOwnedYN) {
+       return new CustomerInfo(customerName,gender,age,education,martialStatus,familyCount,
+               childCount,incomeType,incomeClass,occupation,orgType,employedYears,houseOwnedYN,
+               dwellingType,hosingType,residenceClass,houseAge,carOwnedYN);
 
+    }
 
+    protected CustomerInfo() {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerInfo that)) return false;
+        return CustomerId != null && Objects.equals(CustomerId, that.CustomerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(CustomerId);
+    }
 }
