0e478bd078fa097322d44e09245e664a319964bf
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-28 06:32:00 +0900

    #16 - thymeleaf decoupled logic 설
    java code로 thymeleaf3 decoupled logic을 사용자 applicaition property로 제어할 수 있게 설정 추가

--- src/main/java/com/verystrong/car_loan_project/CarLoanProjectApplication.java
+++ src/main/java/com/verystrong/car_loan_project/CarLoanProjectApplication.java
@@ -2,7 +2,9 @@ package com.verystrong.car_loan_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class CarLoanProjectApplication {
 
