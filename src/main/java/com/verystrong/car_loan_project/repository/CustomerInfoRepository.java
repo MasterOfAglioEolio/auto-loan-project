095fa7859214855173804db62f2e7c480e4f6baf
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-28 04:36:42 +0900

    #1 - 회원정보 리포지토리 수정
    QueryDSL 뺌

--- src/main/java/com/verystrong/car_loan_project/repository/CustomerInfoRepository.java
+++ src/main/java/com/verystrong/car_loan_project/repository/CustomerInfoRepository.java
@@ -1,7 +1,22 @@
 package com.verystrong.car_loan_project.repository;
 
 import com.verystrong.car_loan_project.domain.CustomerInfo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< Updated upstream
 
 public interface CustomerInfoRepository extends JpaRepository<CustomerInfo, Long> {
+=======
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerInfoRepository extends
+        JpaRepository<CustomerInfo, Long>
+//        QuerydslPredicateExecutor<CustomerInfo> // TODO : 검색기능 추가인데 필요할까? 일단 보류
+//        QuerydslBinderCustomizer<QCustomerinfo>
+{
+
+>>>>>>> Stashed changes
 }
