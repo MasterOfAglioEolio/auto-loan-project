f1a7777a7ebad1f6f42032ac0fa7912744ac59a0
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-28 06:33:19 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트 까지 한번에 적용

--- src/main/java/com/verystrong/car_loan_project/controller/CustomerInfoController.java
+++ src/main/java/com/verystrong/car_loan_project/controller/CustomerInfoController.java
@@ -4,6 +4,7 @@ package com.verystrong.car_loan_project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,5 +18,14 @@ public class CustomerInfoController {
     {
         map.addAttribute("customerinfo", List.of());
         return "customerinfo/index";
+    }
+
+    @GetMapping("/{customerId}")
+    public String customerinfo(@PathVariable Long articleId, ModelMap map)
+    {
+        map.addAttribute("customerinfo",null);
+        //map.addAttribute("customerloaninfo",List.of());;
+
+        return "customerinfo/detail";
     }
 }
