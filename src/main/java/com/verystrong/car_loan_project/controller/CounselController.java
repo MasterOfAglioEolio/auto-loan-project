698dc6daa247994a051aeaab9de71603541eb450
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2024-01-09 10:32:07 +0900

    test

--- src/main/java/com/verystrong/car_loan_project/controller/CounselController.java
+++ src/main/java/com/verystrong/car_loan_project/controller/CounselController.java
@@ -17,7 +17,7 @@ public class CounselController {
     private final ChatService chatService;
 
     //chat-gpt 와 간단한 채팅 서비스 소스
-    @PostMapping("")
+    @PostMapping()
     public String test(@RequestBody String question) {
         return chatService.getChatResponse(question);
         //\n\nAs an AI language model, I don't have feelings, but I'm functioning well. Thank you for asking. How can I assist you today?
