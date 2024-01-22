a8a5f33b3359388d7294d9e8c07523b88e3ae0cb
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2024-01-22 16:32:09 +0900

    #49 - 회원가입을 위한 SpringSecurity 설정

--- src/main/java/com/verystrong/car_loan_project/config/SecurityConfig.java
+++ src/main/java/com/verystrong/car_loan_project/config/SecurityConfig.java
@@ -2,16 +2,44 @@ package com.verystrong.car_loan_project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth-> auth.anyRequest().permitAll());
+//                        .requestMatchers(
+//                                HttpMethod.GET,
+//                                "/",
+//                                "/accounts",
+//                                "/accounts/new"
+//                        ).permitAll());
+//                ).formLogin(login -> login
+//                        .loginPage("/members/login")
+//                        .loginProcessingUrl("/login-process")
+//                        .usernameParameter("userid")
+//                        .passwordParameter("pw")
+//                        .defaultSuccessUrl("/loginSuccess", true)
+//                        .failureForwardUrl("/login?error")
+//                        .permitAll())
+//                .logout(Customizer.withDefaults());
+        return http.build();
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        return http
-//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-//                .formLogin(Customizer.withDefaults()).build();
-//    }
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+}
 
 }
