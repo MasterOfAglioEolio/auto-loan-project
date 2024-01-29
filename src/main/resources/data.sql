3302abe160e975f614f090961224ddf4f142e613
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2024-01-29 22:56:36 +0900

    #55 - 엄

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,5 @@
 # select * from customer_info
-INSERT INTO customer_info (gender, age, education, marital_status, family_count, child_count, income_type, income_class, occupation, org_type, employed_years, house_ownedyn, dwelling_type, housing_type, residence_class, car_ownedyn) VALUES ('Woman', '21.0', 'GraduateSchool', 'Single', '2', '0', 'Service', 'E', 'General_work', 'Trade', '2.3', 'Y', 'Self', 'Apartment', 'Good', 'N');
-# INSERT INTO article(title, content) VALUES('가가가가','1111');
-# INSERT INTO article(title, content) VALUES('나나나나','2222');
-# INSERT INTO article(title, content) VALUES('다다다다','3333');
-#
-# INSERT INTO article(title, content) VALUES('당신의 인생 영화는?','댓글 고');
-# INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?','댓글 고고');
-# INSERT INTO article(title, content) VALUES('당신의 취미는?','댓글 고고고');
-#
-# INSERT INTO comment(article_id, nickname, body) VALUES('4','Park','굿 윌 헌팅');
-# INSERT INTO comment(article_id, nickname, body) VALUES('4','kim','아이 엠 샘');
-# INSERT INTO comment(article_id, nickname, body) VALUES('4','Choi','쇼생크 탈출');
-#
-# INSERT INTO comment(article_id, nickname, body) VALUES('5','Park','치킨');
-# INSERT INTO comment(article_id, nickname, body) VALUES('5','kim','샤브샤브');
-# INSERT INTO comment(article_id, nickname, body) VALUES('5','Choi','초밥');
-#
-# INSERT INTO comment(article_id, nickname, body) VALUES('6','Park','조깅');
-# INSERT INTO comment(article_id, nickname, body) VALUES('6','kim','유튜브 시청');
-# INSERT INTO comment(article_id, nickname, body) VALUES('6','Choi','독서');
+INSERT INTO account(account_id, email, password, role) VALUES ('jyjy1284','asdf@asdf',1234,'USER');
+INSERT INTO customer_info (account_id, gender, age, education, marital_status, family_count, child_count, income_type, income_class, occupation, org_type, employed_years, house_ownedyn, dwelling_type, housing_type, residence_class, car_ownedyn) VALUES ('jyjy1284','Woman', '21.0', 'GraduateSchool', 'Single', '2', '0', 'Service', 'E', 'General_work', 'Trade', '2.3', 'Y', 'Self', 'Apartment', 'Good', 'N');
 
 
