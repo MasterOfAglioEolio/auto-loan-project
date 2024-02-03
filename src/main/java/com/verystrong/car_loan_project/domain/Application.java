c60941a066b60da2276e59fbb1903b3781c1dc55
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2024-02-04 06:49:34 +0900

    #42 - 심사 신청을 위한 필요없는 entity 정리

--- src/main/java/com/verystrong/car_loan_project/domain/Application.java
+++ src/main/java/com/verystrong/car_loan_project/domain/Application.java
@@ -33,39 +33,44 @@ public class Application{
     @Column(nullable = false)
     private String accountId;
 
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private String name;
 
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private String cellPhone;
 
+    @Column(nullable = false)
     private BigDecimal carPrice; // 차량 가격 //TODO : 뷰에 추가하고 Deposit 비율로 LoanAmount(hopeAmount 바꾸기) 계산
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private LoanType loanType;
 
+    @Column(nullable = false)
     private Integer deposit;
 
+    @Column(nullable = false)
     private Integer loanTerm;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private InterestType interestType;
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime maturity; //만기 TODO: 신청일자 기준 + loanTerm 으로 계산
+//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+//    private LocalDateTime maturity; //만기 TODO: 신청일자 기준 + loanTerm 으로 계산
 
-    @Column(columnDefinition = "decimal(15,2)")
-    private BigDecimal hopeAmount; // 대출 신청 금액 (TODO : 계산해서 알려주기)
+//    @Column(columnDefinition = "decimal(15,2)")
+//    private BigDecimal hopeAmount; // 대출 신청 금액 (TODO : 계산해서 알려주기)
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 //    @Column(nullable = false)
-    private LocalDateTime appliedAt; // 신청일자
+//    private LocalDateTime appliedAt; // 신청일자
 
-    @Column(columnDefinition = "decimal(15,2)")
-    private BigDecimal approvalAmount;  // TODO : 삭제 고려
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime contractedAt; //약정일자
+//    @Column(columnDefinition = "decimal(15,2)")
+//    private BigDecimal approvalAmount;  // TODO : 삭제 고려
+//
+//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+//    private LocalDateTime contractedAt; //약정일자
 
 
 }
